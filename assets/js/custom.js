63656bc1e5d4ae3f7d3abe943d2d120ba8e5ef05
Remigius Kalimba <kalimbatech@gmail.com>
2017-12-18 13:15:51 +0200

    scrolling update 1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,65 @@
-function a() {
-  console.log("ASDASDA");
+var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+function animateOnAction(tag, animationName) {
+  $(tag).addClass(animationName).one(animationEnd, () => {
+      $(tag).removeClass(animationName);
+    });
+  }
+
+function removeClass(tag, className) {
+  $(tag).removeClass(className);
 }
 
-a();
+  $('document').ready(() => {
+
+    // Smooth scroll
+    $("#topBTN").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#aboutmeSection").offset().top - 110
+        }, 2000);
+    });
+
+    // Smooth scroll
+    $("#aboutBTN").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#aboutmeSection").offset().top - 110
+        }, 2000);
+    });
+
+    // Smooth scroll
+    $("#experienceBTN").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#experienceSection").offset().top - 110
+        }, 2000);
+    });
+
+    // Smooth scroll
+    $("#projectsBTN").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projectsSection").offset().top - 110
+        }, 2000);
+    });
+
+    // Smooth scroll
+    $("#skillsBTN").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#skillsSection").offset().top - 110
+        }, 2000);
+    });
+
+    // Smooth scroll
+    $("#acomplishmentsBTN").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#acomplishmentsSection").offset().top - 110
+        }, 2000);
+    });
+
+    $(window).scroll(function() {
+      console.log(this.scrollY);
+      if (this.scrollY > 200 && this.scrollY < 300 ) {
+
+        animateOnAction('h1.display-5', 'animated rubberBand');
+      }
+  });
+
+  });
